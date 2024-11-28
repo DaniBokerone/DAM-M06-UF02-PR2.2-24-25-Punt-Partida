e7f306fbb804591d140e571e6244ec7d51073b82
DaniBokerone <danibokerone@gmail.com>
2024-11-28 17:39:27 +0100

    Added Jpa version
    Theres an error when adding ciutadans, they doesnt add correctly to the object Ciutat

---
+++ src/main/java/com/project/Ciutada.java
@@ -0,0 +1,95 @@
+package com.project;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ciutadans")
+public class Ciutada {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="ciutadaId", unique=true, nullable=false)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name="ciutatId")
+    private Ciutat ciutat;
+    private String nom;
+    private String cognom;
+    private int edat;
+
+    public Ciutada() {}
+
+
+    public Ciutada(String nom, String cognom, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+    }
+
+    public long getCiutadaId() {
+        return id;
+    }
+
+    public Ciutat getCiutat() {
+        return ciutat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+    
+    public int getEdat() {
+        return edat;
+    }
+
+    public void setCiutadaId(long id) {
+        this.id = id;
+    }
+
+    public void setCiutat(Ciutat ciutat) {
+        this.ciutat = ciutat;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciutada - " + this.getCiutadaId() + ": " + this.getNom() + " "+ this.getCognom() + " , edat " + this.getEdat() + " anys";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Ciutada ciutada = (Ciutada) o;
+        return id == ciutada.id;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+    
+}
---
+++ src/main/java/com/project/Ciutat.java
@@ -0,0 +1,116 @@
+package com.project;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ciutats")
+public class Ciutat {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="ciutatId", unique=true, nullable=false)
+    private long ciutatId;
+    private String nom;
+    private String pais;
+    private int codiPostal;
+
+    @OneToMany(mappedBy = "ciutat", 
+    fetch = FetchType.EAGER)
+    private Set<Ciutada> ciutadans = new HashSet<>();
+
+    public Ciutat() {}
+
+    public Ciutat(String nom, String pais, int codiPostal) {
+        this.nom = nom;
+        this.pais = pais;
+        this.codiPostal = codiPostal;
+    }
+
+    public long getCiutatId() {
+        return ciutatId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public int getPoblacio() {
+        return codiPostal;
+    }
+
+    public void setCiutatId(long ciutatId) {
+        this.ciutatId = ciutatId;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setPoblacio(int codiPostal) {
+        this.codiPostal = codiPostal;
+    }
+
+    public Set<Ciutada> getCiutadans() {
+        return ciutadans;
+    }
+
+    public void setCiutadans(Set<Ciutada> ciutadans) {
+        if (ciutadans != null) {
+            ciutadans.forEach(this::addCiutada);
+        }
+    }
+
+    public void addCiutada(Ciutada ciutada) {
+        ciutadans.add(ciutada);
+        ciutada.setCiutat(this);
+    }
+
+    public void removeCiutada(Ciutada ciutada) {
+        ciutadans.remove(ciutada);
+        ciutada.setCiutat(null);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (Ciutada ciutada : ciutadans) {
+            if (str.length() > 0) {
+                str.append(" | ");
+            }
+            str.append(ciutada.getNom());
+        }
+        return "Ciutat - " + this.getCiutatId() + ": " + this.getNom() + " , pais: " + this.getPais() + " , Ciutadans: [ " + str + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Ciutat ciutat = (Ciutat) o;
+        return ciutatId == ciutat.ciutatId;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Long.hashCode(ciutatId);
+    }
+}
